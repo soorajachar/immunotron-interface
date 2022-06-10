a8312cf8f955f722025f3f2db3e8327201f342b7
Wahlsten <mlwahl@umich.edu>
2022-06-09 21:40:07 -0400

    Merged adding of Anagha's protocol with allowing uneven 384-well plates

--- immunotron-interface/GUI_robotInterface.py
+++ immunotron-interface/GUI_robotInterface.py
@@ -24,7 +24,8 @@ else:
 experimentTypeDict = {
         'Supernatant (Sooraj)':1,
         'Supernatant+Fix/Perm (Madison)':2,
-        'Reverse Plating (Anagha)':3
+        'Reverse Plating (Anagha)':3,
+        'Supernatant+LD/Ab/Fix/Perm (Anagha)':4
         }
 #Root class; handles frame switching in gui
 class MainApp(tk.Tk):
