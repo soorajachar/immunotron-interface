34197dc8208149c10240ff98abab7f4bfef920fc
unknown <NCImantis1@NCI-02218048-L.nci.nih.gov>
2023-06-05 10:52:52 -0400

    modified .bat

--- immunotron-interface/run_robotInterfaceGUI.bat
+++ immunotron-interface/run_robotInterfaceGUI.bat
@@ -1,3 +1,3 @@
 call C:\ProgramData\anaconda3\Scripts\activate.bat
-start /MIN cmd /k  python .\GUI_robotInterface.py
 start /MIN cmd /k  python .\GUI_containerStatus.py
+start /MIN cmd /k  python .\GUI_robotInterface.py
